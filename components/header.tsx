6c46563898e8b55ed961e469385c313f037c3764
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-12-26 12:39:30 +0100

    Fix for multiple `PageLink` that have an `activeClassName` on dynamic routes (#19)
    * added dynamically computed routes
    
    * using a record to prevent update mistakes
    
    * fixed an issue that made all page link to dynamic route match when it should not

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,4 @@
+import { path } from "../pages"
 import { PageLink } from "../sources"
 
 export const Header = () => {
@@ -6,22 +7,32 @@ export const Header = () => {
       <nav>
         <ul>
           <li>
-            <PageLink path="/" activeClassName="active">
+            <PageLink path={path.home} activeClassName="active">
               Home
             </PageLink>
           </li>
           <li>
-            <PageLink path="/about" activeClassName="active">
+            <PageLink path={path.about} activeClassName="active">
               About
             </PageLink>
           </li>
           <li>
-            <PageLink path="/users/2" activeClassName="active">
+            <PageLink path={path.user.computed({user: "1"})} activeClassName="active">
+              User#1
+            </PageLink>
+          </li>
+          <li>
+            <PageLink path={path.user.computed({user: "2"})} activeClassName="active">
               User#2
             </PageLink>
           </li>
           <li>
-            <PageLink path="/users?sort=email" activeClassName="active">
+            <PageLink path={path.user.computed({user: "3"})} activeClassName="active">
+              User#3
+            </PageLink>
+          </li>
+          <li>
+            <PageLink path={path.users} activeClassName="active">
               All Users
             </PageLink>
           </li>
