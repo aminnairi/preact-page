5ad0d7e6d16d94f72d2bdb21a8613d30b6bdc6de
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-12-26 12:12:16 +0100

    using a record to prevent update mistakes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,25 +5,40 @@ import { UserPage } from "./user";
 import { UsersPage } from "./users";
 import { OldPage } from "./old"
 
+export const path = {
+  home: "/home",
+  about: "/about",
+  users: "/users",
+  old: "/old",
+  user: {
+    plain: "/user/:user",
+    computed(parameters: Record<string, string> = {}) {
+      return Object.entries(parameters).reduce((computedPath, [parameterName, parameterValue]) => {
+        return computedPath.replace(`:${parameterName}`, parameterValue)
+      }, this.plain)
+    }
+  }
+}
+
 export const pages: PagesInterface = [
   {
-    path: "/",
+    path: path.home,
     element: <HomePage />
   },
   {
-    path: "/about",
+    path: path.about,
     element: <AboutPage />
-  },
+    },
   {
-    path: "/users",
+    path: path.users,
     element: <UsersPage />
-  },
+    },
   {
-    path: "/users/:user",
+    path: path.user.plain,
     element: <UserPage />
-  },
+    },
   {
-    path: "/old",
+    path: path.old,
     element: <OldPage />
   }
 ]
