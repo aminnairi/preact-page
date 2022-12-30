484c24e9c827d31528bd053c9aa01016ce441e4d
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-12-30 21:37:40 +0100

    added tsconfig paths for vite

--- examples/client-side-rendering/vite.config.ts
+++ examples/client-side-rendering/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite"
 import { preact } from "vite-plugin-preact"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     preact()
   ],
   root: "sources"
