1cd539f49c52c6b3ad37252b2392ae89aa7049cd
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-01-01 20:16:39 +0100

    added metas

--- examples/client-side-rendering/sources/pages/index.tsx
+++ examples/client-side-rendering/sources/pages/index.tsx
@@ -6,14 +6,23 @@ import OldPage from "./old"
 export const pages: PagesInterface = [
   {
     path: "/",
-    element: <HomePage />
+    element: <HomePage />,
+    metas: [
+      () => ({ name: "robots", content: "nofollow, noindex" })
+    ]
   },
   {
     path: "/old",
-    element: <OldPage />
+    element: <OldPage />,
+    metas: [
+      () => ({ name: "robots", content: "nofollow, noindex" })
+    ]
   },
   {
     path: "/user/:user",
-    element: <UserPage />
+    element: <UserPage />,
+    metas: [
+      () => ({ name: "robots", content: "nofollow, noindex" })
+    ],
   }
 ]
