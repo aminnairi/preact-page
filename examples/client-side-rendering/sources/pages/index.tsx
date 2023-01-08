d47d5b399e2f338b56ce9dd9062f6a1f4990107c
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-01-08 20:36:29 +0100

    removed nofollow and noindex

--- examples/client-side-rendering/sources/pages/index.tsx
+++ examples/client-side-rendering/sources/pages/index.tsx
@@ -8,21 +8,21 @@ export const pages: PagesInterface = [
     path: "/",
     element: <HomePage />,
     metas: [
-      () => ({ name: "robots", content: "nofollow, noindex" })
+      () => ({ name: "robots", content: "follow, index" })
     ]
   },
   {
     path: "/old",
     element: <OldPage />,
     metas: [
-      () => ({ name: "robots", content: "nofollow, noindex" })
+      () => ({ name: "robots", content: "follow, index" })
     ]
   },
   {
     path: "/user/:user",
     element: <UserPage />,
     metas: [
-      () => ({ name: "robots", content: "nofollow, noindex" })
+      () => ({ name: "robots", content: "follow, index" })
     ],
   }
 ]
